858c051e7bea2ee8ddc2207a3a33fd4e0917ccbd
Sofía Mallarino <ms.mallarino@gmail.com>
2022-06-29 22:26:24 -0300

    commit preentrega

--- js/main.js
+++ js/main.js
@@ -1,126 +1,27 @@
-// const productos = [
-//     {
-//         "Id": 1,
-//         "Nombre": "Limpiador",
-//         "Precio": 359,
-//         "Categoria": "Limpieza"
-
-//     },
-//     {
-//         "Id": 2,
-//         "Nombre": "Serum",
-//         "Precio": 680,
-//         "Categoria": "Activos"
-
-//     },
-//     {
-//         "Id": 3,
-//         "Nombre": "Hidratante",
-//         "Precio": 520,
-//         "Categoria": "Hidratantes"
-
-//     },
-//     {
-//         "Id": 4,
-//         "Nombre": "Tratamiento",
-//         "Precio": 450,
-//         "Categoria": "Activos"
-
-//     },
-//     {
-//         "Id": 5,
-//         "Nombre": "SPF",
-//         "Precio": 890,
-//         "Categoria": "Protectores"
-
-//     }
-// ]
-
-
-// function ingresarUsuario() {
-//     let ingresarUsuario = prompt("Hola! Bienvenida/o a nuestra tienda \nPorfavor ingrese su usuario.");
-//     alert("Hola " + ingresarUsuario + "," + " por favor seleccione los productos que desea en la siguiente página:");
-// }
-
-// ingresarUsuario()
-
-// function ingresoProductos() {
-//     let ingreso = prompt("Ingresa el número correspondiente a la opción que deseas. \n 1-Limpiador $359 \n 2-Serum $680 \n 3-Hidratante $520 \n 4-Tratamiento $450 \n 5-Pantalla solar SPF +50 $890");
-//     while (ingreso != "SALIR" && ingreso != "salir") {
-//         switch (ingreso) {
-//             case "1":
-//                 alert("Seleccionaste Limpiador")
-//                 break;
-//             case "2":
-//                 alert("Seleccionaste Serum");
-//                 break;
-//             case "3":
-//                 alert("Seleccionaste Hidratante");
-//                 break;
-//             case "4":
-//                 alert("Seleccionaste Tratamiento");
-//                 break;
-//             case "5":
-//                 alert("Seleccionaste Pantalla solar SPF +50");
-//                 break;
-//             default:
-//                 alert("Opcion no valida");
-//                 break;
-//         }
-//         ingreso = prompt("Ingresa el número correspondiente a la opción que deseas. \n 1-Limpiador $359 \n 2-Serum $680 \n 3-Hidratante $520 \n 4- Tratamiento $450 \n 5-Pantalla solar SPF +50 $890 \n\n Para salir ingresa SALIR")
-//     }
-// }
-// ingresoProductos()
-
-
-// const muestrasElegidas = [];
-// let cantidad = 2;
-
-// do {
-//     let entrada = prompt("Con tu compra ofrecemos muestras gratis de productos, selecciona hasta 2 de las siguientes opciones: \n 1-Foam Cleanser piel mixta/grasa \n 2-Foam Cleanser piel sensible/seca \n 3-Cleansing Balm \n 4-Exfoliante BHA+AHA ");
-//     muestrasElegidas.push(entrada);
-// } while (muestrasElegidas.length != cantidad)
-// alert("Tus opciones seleccionadas son " + muestrasElegidas + ". Gracias por comprar con nosotras :)");
-
-
-// const resultado = productos.find((el) => el.Nombre === "Serum")
-// const resultado2 = productos.find((el) => el.Id === "6")
-// const resultado3 = productos.find((el) => el.Precio > 1000)
-
-// const resultado4 = productos.filter((el) => el.Precio <= 600)
-
-
-// console.log(resultado)
-// console.log(resultado2)
-// console.log(resultado3)
-// console.log(resultado4)
-
-// const preciosActualizados = productos.map((el) => {
-//     return {
-//         Nombre: el.Nombre,
-//         Precio: el.Precio * 1.5
-//     }
-// })
-
-// console.log(preciosActualizados)
 
 // --------BOTONERA--------
-
-let contador = document.querySelector('#contador');
-let btnMas = document.querySelector('#aumentar')
-let btnMenos = document.querySelector('#disminuir')
+let contador1 = document.querySelector('#contador1');
+let contador2 = document.querySelector('#contador2')
+let contador3 = document.querySelector('#contador3')
+let btnMas1 = document.querySelector('#aumentar1')
+let btnMas2 = document.querySelector('#aumentar2')
+let btnMas3 = document.querySelector('#aumentar3')
+let btnMenos1 = document.querySelector('#disminuir1')
+let btnMenos2 = document.querySelector('#disminuir2')
+let btnMenos3 = document.querySelector('#disminuir3')
     ;
+
+
 let cont = 0
 function suma(num) {
-    cont = cont += num;
+    cont += num
     contador.innerHTML = cont;
 }
 
 function resta(num) {
-    cont = cont -= num;
+    cont -= num
     contador.innerHTML = cont;
 }
-
 
 
 btnMas.addEventListener('click', () => {
@@ -129,3 +30,27 @@ btnMas.addEventListener('click', () => {
 btnMenos.addEventListener('click', () => {
     resta(1);
 })
+
+
+
+const productos = [
+    { id: 1, producto: "Crema Antioxidante", precio: 1800 },
+    { id: 2, producto: "Serum BHA", precio: 1600 },
+    { id: 3, producto: "Espuma de Limpieza", precio: 700 },
+
+];
+
+const guardarLocal = (clave, valor) => {
+    localStorage.setItem(clave, valor);
+};
+
+//Almacenar producto por producto
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
+// o almacenar array completo
+guardarLocal("listaProductos", JSON.stringify(productos));
+
+let lista = JSON.parse(localStorage.getItem("listaProductos"));
+console.log(lista);
+console.log(productos);
